6aaac980f4e98e1084fdd12cca50262b9193324c
Raj Pandya <raj.pandya@wealthsimple.com>
2025-07-18 21:37:46 -0400

    get basic api working

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from purchase-request-site!")
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
+app = FastAPI(title="Purchase Request Site")
+
+# Mount static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Set up templates
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "title": "Purchase Request Site",
+        "heading": "Enter your purchase request information"
+    })
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
