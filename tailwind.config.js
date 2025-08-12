bba765461f20a27a1d6c38b1fc809a777a3e40e0
Raj Pandya <raj.pandya@wealthsimple.com>
2025-08-11 21:30:53 -0400

    init tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./purchase_request_site/templates/**/*.html",
+    "./purchase_request_site/static/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
